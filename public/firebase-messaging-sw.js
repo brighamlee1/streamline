268cddc9e77dd042f36bf49121e14d52d1098906
Brigham Broadbent <brighamlee1@gmail.com>
2025-02-03 10:06:55 -0600

    fixed: firebase config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,8 +2,15 @@ importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
 
 // Firebase configuration object
-const config = useRuntimeConfig();
-const firebaseConfig = config.public.firebaseConfig;
+const firebaseConfig = {
+    apiKey: "AIzaSyCYJ93_u5miXn3rdhvFQyVGm5CpfJYMZGY",
+    authDomain: "efp-day-orders.firebaseapp.com",
+    projectId: "efp-day-orders",
+    storageBucket: "efp-day-orders.firebasestorage.app",
+    messagingSenderId: "535661012789",
+    appId: "1:535661012789:web:67535be85e4e766463ce7c",
+    measurementId: "G-WG7JT6BTNK"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
